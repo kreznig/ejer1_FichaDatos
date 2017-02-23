fa79475a9fd7bd6619b591c75ea5934a667bd0e4
Miriam Peralta <miiriampc@gmail.com>
2017-02-23 16:15:40 -0500

    Segundo commit: formulario con validaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,8 @@
-
-function envia(){
+function enviar(){
   var name=document.getElementById('name').value;
   var lastname=document.getElementById('lastname').value;
   var dni=document.getElementById('dni').value;
   var adress=document.getElementById('dir').value;
-
-  var datos="<ul><li>Nombre: "+name+"</li><li>Apellidos: "+lastname+"</li><li>DNI: "+dni+"</li><li>Dirección: "+adress+"</li></ul>";
+  var datos= "<ul><li>Nombre: "+name+"</li><li>Apellidos: "+lastname+"</li><li>DNI: "+dni+"</li><li>Dirección: "+adress+"</li></ul>";
   document.getElementById("ficha").innerHTML=datos;
 }
